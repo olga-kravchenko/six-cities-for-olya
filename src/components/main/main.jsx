4c5186388e0da8e66a891460ac06a6fb5692e6d0
Olga <olga19972010@yandex.by>
2021-11-10 17:13:00 +0300

    2.1 Created components for all pages of the application.

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import RoomCard from "../room/room-card";
+import RoomCard from "../room-card/room-card";
 
 const Main = (props) => {
   const {roomQuantity} = props;
