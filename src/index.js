d5bcea9735bf873b7ac24e70915f01b3fee276f6
Olga <olga19972010@yandex.by>
2021-11-20 11:50:09 +0300

    5.1 Connected redux to project.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 import {generateOffer} from "./mocks/offers";
 import App from "./components/app/app";
+import {reducer} from "./store/reducer";
 
 const OFFER_QUANTITY = 5;
 const offers = new Array(OFFER_QUANTITY).fill(null).map(generateOffer);
 const isLogged = true;
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-    <App
-      offerQuantity={OFFER_QUANTITY}
-      offers={offers}
-      isLogged={isLogged}
-    />,
+    <Provider store={store}>
+      <App
+        offerQuantity={OFFER_QUANTITY}
+        offers={offers}
+        isLogged={isLogged}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
