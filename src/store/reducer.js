2d2b6ef716d7ebcc826f1e9281338c008eba1d32
Olga <olga19972010@yandex.by>
2021-11-21 17:25:41 +0300

    5.1 Refactoring reducer.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,30 +1,27 @@
 import {ActionType} from "./action";
 import {generateOffer} from "../mocks/offers";
 
-const OFFER_QUANTITY = 5;
+const OFFER_QUANTITY = 15;
 const offers = new Array(OFFER_QUANTITY).fill(null).map(generateOffer);
+const offersParis = [...offers].filter((e) => e.city.name === `Paris`);
+const offersCologne = [...offers].filter((e) => e.city.name === `Cologne`);
 
 const initialState = {
-  city: {
-    "location": {
-      "latitude": 52.370216,
-      "longitude": 4.895168,
-      "zoom": 10
-    },
-    "name": `Amsterdam`,
-  },
-  offerList: [...offers].filter((e) => e.city.name === `Amsterdam`),
+  city: `Paris`,
+  offerList: offers.filter((e) => e.city.name === `Paris`),
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionType.CITY_CHANGE:
-      return {...state, city: state.city + action.payload};
+      return {...state, city: action.payload};
 
     case ActionType.OFFER_FILLING:
-      return {...state, offerList: state.offerList + action.payload};
+      return {...state, offerList: offers.filter((e) => e.city.name === action.payload)};
+
+    default:
+      return state;
   }
-  return state;
 };
 
 export {reducer};
